2dfbc5998c0dd8261673de418cb4fed10f1223f3
Yanying Wang <yanyingwang1@gmail.com>
2022-04-02 20:23:38 +0800

    nil

--- half.sh
+++ half.sh
@@ -5,6 +5,7 @@ export XDG_RUNTIME_DIR="/run/user/1000"
 
 DISPLAY=:0 notify-send -u low -t 1 -i "${HOME}/hours-chime/fav.png" "半点提醒：现在时刻${TIME}。"
 play -v 0.9 "${HOME}/hours-chime/audio/bell/bell-s.mp3"
+# play -v 16 "${HOME}/hours-chime/audio/westminster/half-hour.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellCccc.mp3"
 # play -v 3 "${HOME}/hours-chime/audio/bell/chime-sound.mp3"
 # play -v 1.2 "${HOME}/hours-chime/audio/bell/notification-s.mp3"
--- main.sh
+++ main.sh
@@ -7,8 +7,9 @@ DISPLAY=:0 notify-send -u low -t 3 -i "${HOME}/hours-chime/fav.png" "整点提�
 # pactl set-sink-volume @DEFAULT_SINK@ -10%
 # pactl set-sink-volume @DEFAULT_SINK@ +10%
 
+# play -v 8 "${HOME}/hours-chime/audio/westminster/first-quarter.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellAa.mp3"
-play "${HOME}/hours-chime/audio/chunch-bell/church-bellAaa.mp3"
+# play -v 0.8 "${HOME}/hours-chime/audio/chunch-bell/church-bellAaa.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellAaaa.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellB.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellB.mp3"
@@ -18,7 +19,7 @@ play "${HOME}/hours-chime/audio/chunch-bell/church-bellAaa.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellC.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellC.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellC.mp3"
-# play "${HOME}/hours-chime/audio/chunch-bell/church-bellCc.mp3"
+play "${HOME}/hours-chime/audio/chunch-bell/church-bellCc.mp3"
 # play "${HOME}/hours-chime/audio/chunch-bell/church-bellCccc.mp3"
 
 paplay "${HOME}/hours-chime/audio/电台DJ/${HOUR}.wav"
